4ab9bb6648e779e5a333bf4386dd26b701f76610
Skyler Blue Spillers <92972770+skylerblue333@users.noreply.github.com>
2026-08-24 02:23:05 -0500

    test: make Jest server hook callback return void

--- tests/chat.test.ts
+++ tests/chat.test.ts
@@ -18,7 +18,9 @@ function connectClient(): Promise<ClientSocket> {
 }
 
 describe('chat policy', () => {
-  beforeAll((done) => httpServer.listen(0, '127.0.0.1', done));
+  beforeAll((done) => {
+    httpServer.listen(0, '127.0.0.1', done);
+  });
 
   afterEach(() => {
     for (const socket of io.sockets.sockets.values()) socket.disconnect(true);
